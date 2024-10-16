cc50c05898167ef9a789bf7f82d42aa6ae6991e1
Julian <86893049+1Osiris@users.noreply.github.com>
2024-10-16 00:22:57 -0400

    Create HelloWorld.java

---
+++ org/howard/edu/lsp/midterm/question3/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+*  Name: Julian Matthews
+*/
+package org.howard.edu.lsp.midterm.question3;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
